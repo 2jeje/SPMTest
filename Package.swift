190635960fa0218d9b8bc1bd5738070e9f851556
jeje.e <jeje.e@kakaocorp.com>
2021-09-30 14:05:41 +0900

    fix source coe path

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "SPMTest",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,23 +19,22 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-      //  .package(name: "PlayingCard",
-        //         url: "https://github.com/2jeje/SPMTest.git", from: "0.0.2")
+        .package(name: "Alamofire", url: "https://github.com/Alamofire/Alamofire.git", from: "5.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Auth",
-            dependencies: [],
-            path: "Auth"
+            dependencies: [
+                .byName(name: "Alamofire")
+            ]
             ),
         .target(
             name: "User",
             dependencies: [
                 .target(name: "Auth")
-            ],
-            path: "User"
+            ]
         )
     ]
 )
